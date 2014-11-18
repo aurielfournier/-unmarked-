b82f4e9c0e3fc1c2d6b0e9a064a76f4b8d061aa4
aurielfournier <aurielfournier@gmail.com>
2014-11-18 11:21:02 -0600

    it works!! holy wah!

--- formatDistData.R
+++ formatDistData.R
@@ -5,9 +5,15 @@ birds <- read.csv("allbirds.csv",header=T)
 
 birds <- birds[birds$species=="sora",]
 birds14 <- birds[birds$year==2014,]
+birds14$night <- as.factor(birds14$night)
 birds14r1 <- birds14[birds14$round==1,]
 birds14r2 <- birds14[birds14$round==2,]
 birds14r3 <- birds14[birds14$round==3,]
 birds14r4 <- birds14[birds14$round==4,]
 
-birds14r1
+dist.breaks <- c(0,1,2,3,4,5,6,7,8,9,10,11,12)
+
+gd14r1 <- formatDistData(birds14r1, "distance", "impound", dist.breaks, "night" )
+gd14r2 <- formatDistData(birds14r2, "distance", "impound", dist.breaks, "night" )
+gd14r3 <- formatDistData(birds14r3, "distance", "impound", dist.breaks, "night" )
+gd14r4 <- formatDistData(birds14r4, "distance", "impound", dist.breaks, "night" )
