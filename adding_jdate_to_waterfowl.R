04b12f5cb1beb44ba285eaf1a66dc6dce155cced
aurielfournier <aurielfournier@gmail.com>
2015-01-13 14:19:38 -0600

    wrote a script just to add jdate to the waterfowl data so I can keep running it as the rest of the data comes in over th coming weeks

---
+++ adding_jdate_to_waterfowl.R
@@ -0,0 +1,17 @@
+#adding jdate to the waterfowl data
+
+dat <- read.csv('2014_waterfowl.csv')
+dat <- dat[,2:ncol(dat)]
+
+dates <- dat[,c("month","day","year")]
+
+varmonth<-cbind(c(1:12),c(0,31,59,90,120,151,181,212,243,273,304,334))
+juldates<-data.frame(date=as.numeric())
+for(i in 1:nrow(dates)) {juldates[i,1]<-varmonth[which(dates[i,1]==varmonth[,1]),2]+dates[i,2]
+                         
+}
+
+dat$jdate <- juldates$date
+
+
+write.csv(dat,"2014_waterfowl.csv")
