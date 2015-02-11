c2211e3375b7a3db62f4735c6270811de8f667b9
aurielfournier <aurielfournier@gmail.com>
2015-02-11 17:12:23 -0600

    trying to tease apart diff ebird data

--- comparing_ebird_abundnces.R
+++ comparing_ebird_abundnces.R
@@ -20,30 +20,31 @@ ggplot(data=mdat)+
   geom_bar(aes(x=month,y=value),position=position_dodge(), stat="identity", fill="black",colour="black")
 
 
-setwd("~/GitHub/data")
-dat12 <- read.csv('Ebird_2012.csv',header=T)
-dat13 <- read.csv('Ebird_2013.csv',header=T)
-dat14 <- read.csv('Ebird_2014.csv',header=T)
+setwd("~/GitHub/ebird")
+dat12 <- read.delim('Ebird_Mo_2012',skip=12,header=T)
+dat13 <- read.delim('Ebird_Mo_2013',skip=12,header=T)
+dat14 <- read.delim('Ebird_Mo_2014',skip=12,header=T)
+# dat12 <- read.csv('Ebird_2012.csv',header=T)
+# dat13 <- read.csv('Ebird_2013.csv',header=T)
+# dat14 <- read.csv('Ebird_2014.csv',header=T)
 
 
-dat12 <- dat12[dat12$species=="Sora"|dat12$species=="Virginia Rail"|dat12$species=="Yellow Rail",]
-dat13 <- dat13[dat13$species=="Sora"|dat13$species=="Virginia Rail"|dat13$species=="Yellow Rail",]
-dat14 <- dat14[dat14$species=="Sora"|dat14$species=="Virginia Rail"|dat14$species=="Yellow Rail",]
+dat12 <- dat12[dat12$X=="Sora"|dat12$X=="Virginia Rail"|dat12$X=="Yellow Rail",]
+dat13 <- dat13[dat13$X=="Sora"|dat13$X=="Virginia Rail"|dat13$X=="Yellow Rail",]
+dat14 <- dat14[dat14$X=="Sora"|dat14$X=="Virginia Rail"|dat14$X=="Yellow Rail",]
 
-df12 <- dat12[,c(2,10:ncol(dat12))]
-df13 <- dat13[,c(2,10:ncol(dat13))]
-df14 <- dat14[,c(2,10:ncol(dat14))]
 
-mdat12 <- melt(df12)
-cdat12 <- cast(variable ~ species,data=mdat12)
+
+mdat12 <- melt(dat12)
+cdat12 <- cast(variable ~ X,data=mdat12)
 cdat12 <- cdat12[29:41,]
-mdat13 <- melt(df13)
-cdat13 <- cast(variable ~ species,data=mdat13)
+mdat13 <- melt(dat13)
+cdat13 <- cast(variable ~ X,data=mdat13)
 cdat13 <- cdat13[29:41,]
-mdat14 <- melt(df14)
-cdat14 <- cast(variable ~ species,data=mdat14)
+mdat14 <- melt(dat14)
+cdat14 <- cast(variable ~ X,data=mdat14)
 cdat14 <- cdat14[29:41,]
-graph112 <- ggplot(data=mdat12[mdat12$species=="Sora",])+
+graph112 <- ggplot(data=mdat12[mdat12$X=="Sora",])+
   geom_bar(aes(x=variable,y=value),position=position_dodge(), stat="identity", fill="black",colour="black")
 
 graph212 <- ggplot(data=mdat12[mdat12$species=="Virginia Rail",])+
@@ -76,17 +77,21 @@ graph314 <- ggplot(data=mdat14[mdat14$species=="Yellow Rail",])+
 
 grid.arrange(graph114, graph214, graph314)
 
-sora12 <- smooth.spline(cdat12$variable, cdat12$Sora)
-vira12 <- smooth.spline(cdat12$variable, cdat12$"Virginia Rail")
-yera12 <- smooth.spline(cdat12$variable, cdat12$"Yellow Rail")
+cdat12$week <- seq(1,nrow(cdat12),by=1)
+cdat13$week <- seq(1,nrow(cdat13),by=1)
+cdat14$week <- seq(1,nrow(cdat14),by=1)
 
-sora13 <- smooth.spline(cdat13$variable, cdat13$Sora)
-vira13 <- smooth.spline(cdat13$variable, cdat13$"Virginia Rail")
-yera13 <- smooth.spline(cdat13$variable, cdat13$"Yellow Rail")
+sora12 <- smooth.spline(cdat12$week, cdat12$Sora)
+vira12 <- smooth.spline(cdat12$week, cdat12$"Virginia Rail")
+yera12 <- smooth.spline(cdat12$week, cdat12$"Yellow Rail")
 
-sora14 <- smooth.spline(cdat14$variable, cdat14$Sora)
-vira14 <- smooth.spline(cdat14$variable, cdat14$"Virginia Rail")
-yera14 <- smooth.spline(cdat14$variable, cdat14$"Yellow Rail")
+sora13 <- smooth.spline(cdat13$week, cdat13$Sora)
+vira13 <- smooth.spline(cdat13$week, cdat13$"Virginia Rail")
+yera13 <- smooth.spline(cdat13$week, cdat13$"Yellow Rail")
+
+sora14 <- smooth.spline(cdat14$week, cdat14$Sora)
+vira14 <- smooth.spline(cdat14$week, cdat14$"Virginia Rail")
+yera14 <- smooth.spline(cdat14$week, cdat14$"Yellow Rail")
 
 ks.test(cdat12$Sora,cdat14$Sora)
 ks.test(cdat13$Sora,cdat14$Sora)
