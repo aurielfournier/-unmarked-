4f6cab6e72675a5d98606f316c525521505cc92e
aurielfournier <aurielfournier@gmail.com>
2015-02-18 16:50:18 -0600

    working on the ks tests more
    adding normalizing

--- ks_sora_pheno.R
+++ ks_sora_pheno.R
@@ -34,8 +34,10 @@ smoothdf <- rbind(rbind(smoothdf12, smoothdf13),smoothdf14)
 smoothdf$year <- as.factor(smoothdf$year)
 
 
-m <- melt(smoothdf, id=c("year", "x"))
- ct <- cast(x ~ year, data=m)
+msora <- melt(smoothdf, id=c("year", "x"))
+msora$value <- scale(msora$value)
+msora$type <- "count"
+ ctsora <- cast(x ~ year, data=msora)
 
 ks.test(ct$"2013",ct$"2014")
 # p = .0000007226
@@ -46,28 +48,38 @@ ks.test(ct$"2012",ct$"2013")
 ks.test(ct$"2012",ct$"2014")
 # p = 0.0008699
 
-m$week <-ifelse(m$x>=226&m$x<=232,1,
-                 ifelse(m$x>=233&m$x<=239,2,
-                 ifelse(m$x>=240&m$x<=246.9,3,
-                 ifelse(m$x>=247&m$x<=253.9,4,
-                 ifelse(m$x>=254&m$x<=260.9,5,
-                 ifelse(m$x>=261&m$x<=267,6,
-                 ifelse(m$x>=268&m$x<=274,7,
-                 ifelse(m$x>=275&m$x<=281,8,
-                 ifelse(m$x>=282&m$x<=288,9,
-                 ifelse(m$x>=289&m$x<=295,10,
-                 ifelse(m$x>=296&m$x<=300,11,NA)))))))))))
 
-cdat <- cast(week ~ year, data=m, mean )
-
-zero <- as.data.frame(matrix(0, nrow=2,ncol=4))
-colnames(zero) <- colnames(cdat)
-
-abund_week <- rbind(zero,cdat)
+ggplot() + 
+  geom_bar(data=xaxis, aes(x=jdate, y=value), position=position_dodge(), stat="identity", colour="white",size=.5) +
+  geom_line(data=m, aes(x=x, y=value, group=year, colour=year), size=2)
 
 
-ks.test(abund_week$"2013",abund_week$"2012")
-
-ks.test(abund_week$"2014",abund_week$"2013")
-
-ks.test(abund_week$"2014",abund_week$"2012")
+# ignore this, I was tyring to lump to compare to ebird graphs, but that is dumb
+# now I am just going to get the actual day by day data from ebird and compare that
+# like a smart person
+#
+# m$week <-ifelse(m$x>=226&m$x<=232,1,
+#                  ifelse(m$x>=233&m$x<=239,2,
+#                  ifelse(m$x>=240&m$x<=246.9,3,
+#                  ifelse(m$x>=247&m$x<=253.9,4,
+#                  ifelse(m$x>=254&m$x<=260.9,5,
+#                  ifelse(m$x>=261&m$x<=267,6,
+#                  ifelse(m$x>=268&m$x<=274,7,
+#                  ifelse(m$x>=275&m$x<=281,8,
+#                  ifelse(m$x>=282&m$x<=288,9,
+#                  ifelse(m$x>=289&m$x<=295,10,
+#                  ifelse(m$x>=296&m$x<=300,11,NA)))))))))))
+# 
+# cdat <- cast(week ~ year, data=m, mean )
+# 
+# zero <- as.data.frame(matrix(0, nrow=2,ncol=4))
+# colnames(zero) <- colnames(cdat)
+# 
+# abund_week <- rbind(zero,cdat)
+# 
+# 
+# ks.test(abund_week$"2013",abund_week$"2012")
+# 
+# ks.test(abund_week$"2014",abund_week$"2013")
+# 
+# ks.test(abund_week$"2014",abund_week$"2012")
