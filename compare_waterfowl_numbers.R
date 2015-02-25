8565b0ec3f94961d62cadc3fe590ce6843e04a14
aurielfournier <aurielfournier@gmail.com>
2015-02-25 16:08:45 -0600

    tidying waterfowl data

--- compare_waterfowl_numbers.R
+++ compare_waterfowl_numbers.R
@@ -1,63 +1,33 @@
 
-dat <- read.csv('2014_waterfowl.csv')
+dat <- read.csv("2014_waterfowl_tidy_hec.csv")
 #dat <- dat[dat$region!="ne",]
 library(gridExtra)
 library(ggplot2)
-
-cb2 <- c("#a6611a","#018571")
+dat <- dat[dat$variable=="waterfowl",]
+cb2 <- c("#018571","#a6611a")
 
 a <-ggplot()+
-  geom_boxplot(data=dat, aes(region, waterfowl, fill=treat))+
-  ylim(0,10000)+
+  geom_boxplot(data=dat, aes(region, birds_ha, fill=treat))+
+  ylim(0,200)+
   scale_fill_manual(values=cb2)+
-  theme(text=element_text(size=20),
-        legend.text = element_text(colour="black", size=20), #legend text
+  ylab("Ducks per hectare")+
+  theme(plot.title = element_text(colour="black",size=40), #plot title
+        axis.text.x = element_text(colour="black", size=40), #x axis labels
+        axis.text.y = element_text(colour="black",size=40), #y axis labels
+        axis.title.x = element_blank(), #x axis title
+        axis.title.y = element_text(colour="black",size=40), #y axis title
+        legend.text = element_text(colour="black", size=40), #legend text
         legend.title = element_blank(),#legend title
         legend.background = element_rect(fill="white"), #legend background color
-        legend.position = "right",
-        legend.direction= "vertical",
-        legend.key = element_blank(),
+        legend.position = "top",
+        legend.direction= "horizontal",
+        legend.key = element_rect(colour="black", fill="white"),
+        legend.key.width=unit(3,"cm"),
         plot.background = element_rect(fill = "white" ), #plot background color
-        panel.background = element_rect(fill = "white"), #panel background color
-        panel.grid.major.y= element_line(colour="black"), #y axis grid line color
-        panel.grid.major.x = element_line(colour=NA),
-        panel.grid.minor = element_line(colour=NA))
-
-b <- ggplot()+
-  geom_boxplot(data=dat, aes(region, dabblers, fill=treat))+
-  scale_fill_manual(values=cb2)+
-  theme(text=element_text(size=20),
-        legend.text = element_text(colour="black", size=20), #legend text
-        legend.title = element_blank(),#legend title
-        legend.background = element_rect(fill="white"), #legend background color
-        legend.position = "right",
-        legend.direction= "vertical",
-        legend.key = element_blank(),
-        plot.background = element_rect(fill = "white" ), #plot background color
-        panel.background = element_rect(fill = "white"), #panel background color
-        panel.grid.major.y= element_line(colour="black"), #y axis grid line color
-        panel.grid.major.x = element_line(colour=NA),
-        panel.grid.minor = element_line(colour=NA))
+        panel.grid.major= element_line(colour=NA), 
+        panel.grid.minor=element_line(colour=NA),
+        panel.background = element_rect(fill = "white"),
+        axis.line=element_line(colour="black"))
 
 
-d <- ggplot()+
-  geom_boxplot(data=dat, aes(region, mall, fill=treat))+
-  ylim(0,10000)+
-  scale_fill_manual(values=cb2)+
-  theme(text=element_text(size=20),
-        legend.text = element_text(colour="black", size=20), #legend text
-        legend.title = element_blank(),#legend title
-        legend.background = element_rect(fill="white"), #legend background color
-        legend.position = "right",
-        legend.direction= "vertical",
-        legend.key = element_blank(),
-        plot.background = element_rect(fill = "white" ), #plot background color
-        panel.background = element_rect(fill = "white"), #panel background color
-        panel.grid.major.y= element_line(colour="black"), #y axis grid line color
-        panel.grid.major.x = element_line(colour=NA),
-        panel.grid.minor = element_line(colour=NA))
-
-
-grid.arrange(a,b,d,ncol=1)
-
-ttest <- lm(waterfowl ~ treat, data=dat)
+ttest <- lm(birds_ha ~ treat, data=dat)
