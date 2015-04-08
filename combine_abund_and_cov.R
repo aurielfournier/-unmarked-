d1bbcda8e2902fc456eeb31e763576b0514cf8b8
Auriel Fournier <aurielfournier@gmail.com>
2015-04-08 13:08:49 -0500

    adding comments

--- combine_abund_and_cov.R
+++ combine_abund_and_cov.R
@@ -1,29 +1,27 @@
+## my adviser wanted my abundance estimates in the same file as my covariates so he could look at them
+
+## so i merged them together
+
 
 setwd("~/GitHub/data")
+
+## 2012
 abund <- read.csv("abundances_2012.csv")
 c12 <- read.csv("2012r3_cov.csv")
-
-c12 <- c12[,c(1:3,6:15)]
-
-
+c12 <- c12[,c(1:3,6:15)] # taking out covariates that he does not need
 d12 <- merge(abund,c12,by=c("impound"))
 write.csv(d12, "abund_cov_2012.csv", row.names=F)
 
+## 2013
 abund <- read.csv("abundances_2013.csv")
 c13 <- read.csv("2013r3_cov.csv")
-
 c13 <- c13[,c(1:3,6:17)]
-
-
 d13 <- merge(abund,c13,by=c("impound"))
 write.csv(d13, "abund_cov_2013.csv", row.names=F)
 
-
+## 2014
 abund <- read.csv("abundances_2014.csv")
 c14 <- read.csv("2014r3_cov.csv")
-
 c14 <- c14[,c(1:3,7:11)]
-
-
 d14 <- merge(abund,c14,by=c("impound"))
 write.csv(d14, "abund_cov_2014.csv", row.names=F)
