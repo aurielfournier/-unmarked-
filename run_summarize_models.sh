7b53606d4545a425074abe5d691c70030265a3a6
Auriel Fournier <aurielfournier@gmail.com>
2015-04-26 14:55:28 -0500

    updating model summary

--- run_summarize_models.sh
+++ run_summarize_models.sh
@@ -66,6 +66,5 @@ Rscript -e "library(knitr); knit('gdistsamp_14_r4.rmd')"
 
 tail -20 gdistsamp_14_r4.md
 
-printLine tail -15 gdistsamp_12_r1.md | tail -15 gdistsamp_12_r2.md | tail -15 gdistsamp_12_r3.md | tail -15 gdistsamp_13_r1.md | tail -15 gdistsamp_13_r2.md | tail -15 gdistsamp_13_r3.md | tail -15 gdistsamp_13_r4.md | tail -15 gdistsamp_14_r1.md | tail -15 gdistsamp_14_r2.md | tail -15 gdistsamp_14_r3.md | tail -15 gdistsamp_14_r4.md  >> modeling_output.md 
-
+(tail -14 gdistsamp_12_r1.md; tail -14 gdistsamp_12_r2.md; tail -14 gdistsamp_12_r3.md; tail -14 gdistsamp_12_r3.md; tail -14 gdistsamp_13_r1.md; tail -14 gdistsamp_13_r2.md; tail -14 gdistsamp_13_r3.md; tail -14 gdistsamp_13_r4.md; tail -14 gdistsamp_14_r1.md; tail -14 gdistsamp_14_r2.md; tail -14 gdistsamp_14_r3.md; tail -14 gdistsamp_14_r4.md) > modeling_output.md
 
