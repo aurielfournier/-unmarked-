d4f83d55bc8565d6a28691d95a6d1f636cb91b46
aurielfournier <aurielfournier@gmail.com>
2016-02-05 11:52:14 -0600

    formatDistData script updated to dplyr/tidyr

--- 20160115_formatDistData_based_on_sara_comments.R
+++ 20160115_formatDistData_based_on_sara_comments.R
@@ -7,6 +7,8 @@
 # Needed Packages ----------------------------------------------------------------------------------------
 ###
 library(unmarked)
+library(dplyr)
+library(tidyr)
 library(raildata)
 
 
@@ -57,13 +59,16 @@ veg$iry <- paste0(veg$impound,"_",veg$round,"_",veg$year)
 
 veg$iy <- paste0(veg$impound,"_",veg$year)
 
-water = melt(veg[,c( "region","iry","iy", "averagewater")],id=c("iry","iy","region"), na.rm=T)
-veg = melt(veg[,c("iy","int","short")], na.rm=TRUE, id=c("iy"))
 
-castwater <- cast(data=water, iry + iy+ region ~ variable, median)
-castveg <- cast(data=veg, iy ~ variable, median)
+water <- veg[,c( "region","iry","iy", "averagewater")] %>% gather("variable","value",-iry,-iy,-region)
+veg <- veg[,c("iy","int","short")] %>% gather("variable","value",-iy)
 
-all_veg <- merge(castwater, castveg, by="iy", all=FALSE)
+sumwater <- water %>% group_by(iry, iy, region) %>% summarize(median=median(value))
+sumveg <- veg %>% group_by(iy, variable) %>% summarize(median=median(value)) %>% spread(variable, median)
+
+all_veg <- merge(sumwater, sumveg, by="iy", all=FALSE)
+
+colnames(all_veg)[4] <- "averagewater"
 
 data(allsurveys)
 surv <- allsurveys
@@ -74,14 +79,12 @@ surv[surv$impound=="r4/5",]$impound <- "r45"
 
 surv$iry <- paste0(surv$impound,"_",surv$round,"_",surv$year)
 
-mjdate <- melt(surv[,c("jdate","iry")], id="iry")
-mlength <- melt(surv[,c("iry","length")], id="iry")
-clength <- cast(mlength, iry ~ variable, sum)
+mjdate <- surv[,c("jdate","iry")] %>% gather("variable","value",-iry) %>% group_by(variable, iry) %>% summarize(median=median(value)) %>% spread(variable, median)
+clength <- surv[,c("iry","length")] %>% gather("variable","value",-iry) %>% group_by(variable, iry) %>% summarize(sum=sum(value)) %>% spread(variable, sum)
 clength$length <- clength$length*1000
 
-cjdate <- cast(mjdate, iry ~ variable, median)
 
-all_veg_jdate <- merge(all_veg, cjdate, by="iry", all.x=TRUE)
+all_veg_jdate <- merge(all_veg, mjdate, by="iry", all.x=TRUE)
 all_veg_jdate_length <- merge(all_veg_jdate, clength, by="iry",all.x=TRUE)
 
 all_veg_jdate_length$scale_short <- scale(all_veg_jdate_length$short)
@@ -90,15 +93,11 @@ all_veg_jdate_length$scale_averagewater <- scale(all_veg_jdate_length$averagewat
 
 all_veg_jdate_length_year <- separate(all_veg_jdate_length, "iy", into=c("impound","year"), by=-5)
 
-
-
 veg_DONE <- all_veg_jdate_length_year[(all_veg_jdate_length_year$iry %in% gd$iry),]
 
-sora_DONE <- gd[(gd$iry %in% all_veg_jdate_length_year$iry),]
 
 nrow(sora_DONE) == nrow(veg_DONE)
 unique(sora_DONE$iry) == unique(veg_DONE$iry)
-
 
 
 write.csv(sora_DONE, "./data/sora_DONE.csv", row.names=F)
