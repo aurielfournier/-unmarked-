36dd5ca7d3429bf062eec8e11e65355c1610709b
aurielfournier <aurielfournier@gmail.com>
2017-06-19 17:17:07 -0500

    veg done and sora Done is fixed again

--- 20160115_formatDistData_based_on_sara_comments.R
+++ 20160115_formatDistData_based_on_sara_comments.R
@@ -42,7 +42,7 @@ gd$iry <- row.names(gd)
 ####-----------------------------------------------
 # Input covariates ----------------------------------------------------------------------------------------
 ####----------------------------------------------
-
+# allveg[allveg$impound=="kt6"&allveg$year==2014,]
 data(allveg)
 
 veg <- allveg
@@ -61,10 +61,10 @@ veg$iy <- paste0(veg$impound,"_",veg$year)
 
 
 water <- veg[,c( "region","iry","iy", "averagewater")] %>% gather("variable","value",-iry,-iy,-region)
-veg <- veg[,c("iy","int","short")] %>% gather("variable","value",-iy)
+veg <- veg[,c("iy","int","annual")] %>% gather("variable","value",-iy)
 
-sumwater <- water %>% group_by(iry, iy, region) %>% summarize(median=median(value))
-sumveg <- veg %>% group_by(iy, variable) %>% summarize(median=median(value)) %>% spread(variable, median)
+sumwater <- water %>% group_by(iry, iy, region) %>% summarize(median=median(value, na.rm=TRUE))
+sumveg <- veg %>% group_by(iy, variable) %>% summarize(median=median(value, na.rm=TRUE)) %>% spread(variable, median)
 
 all_veg <- merge(sumwater, sumveg, by="iy", all=FALSE)
 
@@ -79,7 +79,7 @@ surv[surv$impound=="r4/5",]$impound <- "r45"
 
 surv$iry <- paste0(surv$impound,"_",surv$round,"_",surv$year)
 
-mjdate <- surv[,c("jdate","iry")] %>% gather("variable","value",-iry) %>% group_by(variable, iry) %>% summarize(median=median(value)) %>% spread(variable, median)
+mjdate <- surv[,c("jdate","iry")] %>% gather("variable","value",-iry) %>% group_by(variable, iry) %>% summarize(median=median(value, na.rm=TRUE)) %>% spread(variable, median)
 clength <- surv[,c("iry","length")] %>% gather("variable","value",-iry) %>% group_by(variable, iry) %>% summarize(sum=sum(value)) %>% spread(variable, sum)
 clength$length <- clength$length*1000
 
@@ -95,10 +95,12 @@ all_veg_jdate_length_year <- separate(all_veg_jdate_length, "iy", into=c("impoun
 
 veg_DONE <- all_veg_jdate_length_year[(all_veg_jdate_length_year$iry %in% gd$iry),]
 
+sora_DONE <- gd[(gd$iry %in% all_veg_jdate_length_year$iry),]
+
 
 nrow(sora_DONE) == nrow(veg_DONE)
 unique(sora_DONE$iry) == unique(veg_DONE$iry)
 
 
-write.csv(sora_DONE, "./data/sora_DONE.csv", row.names=F)
-write.csv(veg_DONE, "./data/veg_DONE.csv", row.names=F)
+write.csv(sora_DONE, "~/data/sora_DONE.csv", row.names=F)
+write.csv(veg_DONE, "~/data/veg_DONE.csv", row.names=F)
